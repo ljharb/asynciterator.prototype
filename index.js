53f0d6d793c9522f0d98931f2da9113f24817613
Jordan Harband <ljharb@gmail.com>
2024-08-27 13:28:42 -0700

    [Fix] use get-intrinsic to get the proper AsyncIteratorPrototype in modern engines

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 
 var hasSymbols = require('has-symbols');
+var GetIntrinsic = require('get-intrinsic');
 
 /* globals AsyncIterator: false */
 
-var asyncIterProto = typeof AsyncIterator === 'function' ? AsyncIterator.prototype : {};
+var asyncIterProto = typeof AsyncIterator === 'function' ? AsyncIterator.prototype : GetIntrinsic('%AsyncIteratorPrototype%', true) || {};
 
 if (hasSymbols() && Symbol.asyncIterator && !(Symbol.asyncIterator in asyncIterProto)) {
 	asyncIterProto[Symbol.asyncIterator] = function () {
